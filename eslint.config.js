5a772610bd67b3c07844d8f90b4f6075b83cbbb2
surface74 <surface.msn@gmail.com>
2024-10-18 12:09:03 +0300

    build: setup eslint 9.12

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+//import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  //{ languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "indent": [
+        "error",
+        "tab"
+      ],
+      "linebreak-style": [
+        "error",
+        "unix"
+      ],
+      "quotes": [
+        "error",
+        "single"
+      ],
+      "semi": [
+        "error",
+        "always"
+      ]
+    },
+  },
+  {
+    ignores: ["eslint.config.js"]
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { message } from './modules/test.js';
 
 message(true);
 
-const greeting: string = 'Hello world!';
+const greeting: string = 'Hi there!';
 
 console.log('greeting: ', greeting);
+console.log(import.meta.dirname);
